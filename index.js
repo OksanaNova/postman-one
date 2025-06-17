819977dc7a4622b5796f66c8a82430314a403032
OksanaNova <o.nova0804@gmail.com>
2025-06-17 11:06:25 -0600

    delete method added

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const recipes = require('./Recipes');
 const users = require('./Users');
-const bodyParser = require('body-parser');
+const { json } = require('body-parser');
+// const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.get('/api/recipes', (req, res) => {
@@ -25,6 +29,24 @@ app.post('/api/recipes', (req, res) => {
     recipes.push(newRecipe);
     res.json(recipes);
 })
+
+app.delete('/api/recipes/:name', (req, res) => {
+    let { name } = req.params;
+    let recipeToBeDeleted = recipes.find(recipe => recipe.name === name);
+
+    if(recipeToBeDeleted) {
+        res.json({
+            message: 'Recipe deleted',
+            recipes: recipes.filter(recipe => recipe.name !== name)
+        })
+    } else {
+        res.status(404)
+        .json({message: `Recipe you are looking for doesn't exist`})
+    }
+})
+
+
+
 
 app.listen(3000, () => {
     console.log(`IT'S WORKING - PORT 3000`)
