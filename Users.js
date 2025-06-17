a7755bc7cc85c9ea469507085250617ad6b84478
OksanaNova <o.nova0804@gmail.com>
2025-06-17 09:07:49 -0600

    users added

---
+++ Users.js
@@ -0,0 +1,52 @@
+const users = [
+    {
+        name: "Anton",
+        gender: "male",
+        age: 40,
+        status: "divorced"
+    },
+        {
+        name: "Alex",
+        gender: "male",
+        age: 42,
+        status: "divorced"
+    },
+        {
+        name: "Mary",
+        gender: "female",
+        age: 38,
+        status: "single"
+    },
+        {
+        name: "Olga",
+        gender: "female",
+        age: 43,
+        status: "married"
+    },
+        {
+        name: "George",
+        gender: "male",
+        age: 41,
+        status: "single"
+    },
+        {
+        name: "Tomas",
+        gender: "male",
+        age: 42,
+        status: "married"
+    },
+        {
+        name: "Oksana",
+        gender: "female",
+        age: 40,
+        status: "divorced"
+    },
+        {
+        name: "Tanya",
+        gender: "female",
+        age: 28,
+        status: "single"
+    }
+]
+
+module.exports = users;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const recipes = require('./Recipes');
+const users = require('./Users');
 
 // console.log(recipes);
 
 app.get('/api/recipes', (req, res) => {
     res.json(recipes);
+})
+
+app.get('/api/users', (req, res) => {
+    res.json(users);
 })
 
 app.listen(3000, () => {
